8d68067d0262deb512dd82bd816127a560e3ccc0
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-01-22 13:43:54 -0800

    current model post was set up to be a router. It has now been corrected.

--- models/post.js
+++ models/post.js
@@ -1,79 +1,40 @@
-const express = require('express');
-const router = express.Router();
-const { Post } = require('../models');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
 
+class Post extends Model {}
 
-router.get('/', async (req, res) => {
-    try {
-        const postData = await Post.findAll();
-        const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('all-posts', { posts });
-        res.status(200).json(postData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
 
-router.get('/:id', async (req, res) => {
-    try {
-        const postData = await Post.findByPk(req.params.id);
-        if (!postData) {
-            res.status(404).json({ message: 'No post found!' });
-            return;
-        }
-        res.status(200).json(postData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.post('/', async (req, res) => {
-    try {
-        const newPost = await Post.create({
-            title: req.body.title,
-            content: req.body.content,
-            userId: req.session.userId 
-        });
-        res.status(200).json(newPost);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedPost = await Post.update(req.body, {
-            where: {
-                id: req.params.id,
-            },
-        });
-
-        if (!updatedPost[0]) {
-            res.status(404).json({ message: 'No post found!' });
-            return;
-        }
-        res.status(200).json(updatedPost);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const postData = await Post.destroy({
-            where: {
-                id: req.params.id,
-            },
-        });
-
-        if (!postData) {
-            res.status(404).json({ message: 'No post found!' });
-            return;
-        }
-        res.status(200).json(postData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-module.exports = router;
+module.exports = Post;
